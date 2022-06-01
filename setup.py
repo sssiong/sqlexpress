a35cf62e5f11d3ab43b4b9c29702097a75078333
sssiong <>
2022-06-01 21:27:15 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (PROJECT_DIR / 'README.md').read_text()
 
 setup(
     name='sqlexpress',
-    version='0.2.0',
+    version='0.2.1',
     author='sssiong',
     description='Package to parse SQL scripts for information',
     long_description=long_description,
